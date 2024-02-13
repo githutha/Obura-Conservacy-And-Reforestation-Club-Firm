d08e9e3ac63a50a31b77eb6ae6584ffd693697f2
Lydia Githutha <125241503+githutha@users.noreply.github.com>
2024-02-13 19:04:37 +0300

    Add files via upload

---
+++ lydia/Obura-Conservacy-And-Reforestation-Club-Firm/README.md
@@ -0,0 +1,3 @@
+# Obura-Conservacy-And-Reforestation-Club-Firm
+Obura conservacy is a fictional firm that specilises in agroforestry 
+day one of attempting to write the proper html for the project
---
+++ lydia/Obura-Conservacy-And-Reforestation-Club-Firm/java.js
@@ -0,0 +1,8 @@
+function openPopUp(){
+    document.getElementById("popup").style.display="block";
+    document.getElementById("overlay").display="block";
+}
+function closePopup(){
+    document.getElementById("popup").style.display="none";
+    document.getElementById("overlay").display="none";   
+}
